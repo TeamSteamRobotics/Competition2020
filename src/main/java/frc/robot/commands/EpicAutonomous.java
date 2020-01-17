bc9f30db2cf98c27117422ed4c326e5e6cf9ca17
FRC5119 <malcolmmaude01@gmail.com>
2020-01-16 20:37:14 -0600

    We made auto

---
+++ src/main/java/frc/robot/commands/EpicAutonomous.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.BallTrackingSubsystem;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class EpicAutonomous extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous.
+   */
+  public EpicAutonomous(DriveSubsystem drive, VisionSubsystem vision, IntakeSubsystem succer, HopperSubsystem hopper, BallTrackingSubsystem tracker, ShooterSubsystem shooter) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(
+      new DriveDistance(drive, -5),
+      new VisionTurn(drive, vision.getTargetX()),
+      (new Shooter(shooter, hopper)).withTimeout(1.5),
+      new GyroTurn(drive, 0),
+      new ParallelRaceGroup(new Intake(succer, hopper, tracker), new DriveDistance(drive, -5)),
+      new DriveDistance(drive, 5),
+      new VisionTurn(drive, vision.getTargetX()),
+      (new Shooter(shooter, hopper)).withTimeout(2.5)
+
+    );
+  }}
---
+++ src/main/java/frc/robot/commands/SuckyAutonomous.java
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class SuckyAutonomous extends SequentialCommandGroup {
+  /**
+   * Creates a new SuckyAutonomous.
+   */
+  public SuckyAutonomous(DriveSubsystem drive, ShooterSubsystem shoot, HopperSubsystem hopper, VisionSubsystem vision){
+
+  
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(
+      new DriveDistance(drive, -5),
+      new VisionTurn(drive, vision.getTargetX()),
+      (new Shooter(shoot, hopper)).withTimeout(1.5)
+      
+    );
+  }
+}
